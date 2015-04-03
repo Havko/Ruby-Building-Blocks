4abefa958e1bf7b80310a567487eec916ebddeff
Steven Havko <stevenhavko@gmail.com>
2015-04-03 14:46:53 -0400

    going home

--- p2stockpicker/stockpicker.rb
+++ p2stockpicker/stockpicker.rb
@@ -1,11 +1,14 @@
 def stockpicker(prices)
 	hash = Hash[prices.map.with_index.to_a]
+	print hash
 	hash = hash.invert
-	hiday = hash.max_by{|k,v| v}[0]
+
 	loday = hash.min_by{|k,v| v}[0]
-	
+	hihash = hash.delete_if {|k, v| k < loday}
+	hiday = hash.max_by{|k,v| v}[0]
+	#loday = hash.min_by{|k,v| v}[0]
 	answer = [loday, hiday]
-	print answer
+	return answer
 end
 
 print stockpicker([17,3,6,9,15,8,6,1,10])
---
+++ p2stockpicker/stockpickerv2.rb
@@ -0,0 +1,5 @@
+def stockpicker(prices)
+	prices.each do |x| 
+end
+
+print stockpicker([17,3,6,9,15,8,6,1,10])
